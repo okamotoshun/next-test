0734a22b67d508fb880b495c363b677454360d64
shun-okamoto <okamons8811@gmail.com>
2023-05-13 21:00:20 +0900

    テストコードの書き方の説明

--- src/__test__/Home.test.tsx
+++ src/__test__/Home.test.tsx
@@ -2,6 +2,15 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Home from '../pages/index'
 
+// describeは関連するテストをブロック単位でまとめるメソット
+// 第1引数にテストブロックの説明
+// 第2引数にテストケースを記述するコールバック関数
+// itは実際のテストを記述する
+// 第1引数はテストの説明
+// 第2引数はテスト内容を記述するコールバック関数
+// render()は引数に指定したJSXをレンダリングする
+// expectはテスト結果を評価するメソット
+
 describe('rendering', () => {
   it('Should render Hello text', () => {
     render(<Home />)
