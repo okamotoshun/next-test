0ffd5f425da4470a97b65a375cf4af6fb939440d
shun-okamoto <okamons8811@gmail.com>
2023-05-13 21:23:36 +0900

    SearchFormのテストを追加

---
+++ src/__test__/SearchForm.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { SearchForm } from '../components/SearchForm'
+
+describe('rendering', () => {
+  it('Should render SearchForm', () => {
+    render(<SearchForm />)
+    expect(screen.getByRole('textbox')).toBeTruthy()
+    expect(screen.getByRole('button')).toBeTruthy()
+  })
+})
